e42fccb5590bfadeaf8dc7fcf85c9fdd64ed75e8
Vladimir Zhelnov <neatek@icloud.com>
2023-02-25 08:17:51 +0300

    lesson_2

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import render_template
+from views.users import users_app
+from views.articles import articles_app
+
 
 app = Flask(__name__)
 
 
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
+
+
 @app.route("/")
 def index():
-    return "Hello web!"
+    return render_template("index.html")
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
