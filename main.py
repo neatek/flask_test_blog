b5653e4977ad78c14084e4f1b6a51638296d5559
Vladimir Zhelnov <neatek@icloud.com>
2023-03-02 14:03:38 +0300

    lesson_3

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask import render_template
 from views.users import users_app
 from views.articles import articles_app
+# from views.auth import auth_app
+from views.auth import login_manager, auth_app
+from models.database import db
 
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+login_manager.init_app(app)
 
+app.register_blueprint(auth_app, url_prefix="/auth")
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
 
@@ -20,3 +29,30 @@ def index():
 @app.route("/hello/<name>")
 def hello(name=None):
     return render_template("hello.html", name=name)
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+    from models import User
+
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
